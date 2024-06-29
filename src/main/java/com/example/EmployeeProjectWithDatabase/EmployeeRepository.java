ef5a56102965f1061ba4f54cd1febe41e1d0477a
kalyanisankpal <sankpalkalyani93@gmail.com>
2024-06-29 16:49:23 +0530

    first commit

--- src/main/java/com/example/EmployeeProjectWithDatabase/EmployeeRepository.java
+++ src/main/java/com/example/EmployeeProjectWithDatabase/EmployeeRepository.java
@@ -1,9 +1,9 @@
 package com.example.EmployeeProjectWithDatabase;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@Repository
+@RepositoryRestResource(collectionResourceRel = "employees", path = "employees")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer>{
 
 }
